ced455cbafe90507d707ff7e48400e8a7b791d0b
tyrkgitgub <tiayorke@hotmail.co.uk>
2023-02-27 17:42:00 +0000

    answers are displayed in markdown. file is created

--- index.js
+++ index.js
@@ -1,48 +1,67 @@
 const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
+const util = require("util");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = () =>
-  inquirer.createPromptModule([
-    {
-      type: "input",
-      name: "title",
-      message: "Please enter the title for the README.md",
-    },
-    {
-      type: "input",
-      name: "descriptiom",
-      message: "Please enter a description of the project",
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Please enter how a user should install the project',
-    }
-    {
-        type: 'input',
-        name: 'usage',
-        message: '',
-    }
-    {
-        type: 'input',
-        name: 'contributing',
-        message: '',
-    }
-    {
-        type: 'input',
-        name: 'tests',
-        message: '',
-    }
-  ]);
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Please enter the title for the README.md",
+  },
+  {
+    type: "input",
+    name: "descriptiom",
+    message: "Please enter a description of the project",
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "Please enter how a user should install the project",
+  },
+  {
+    type: "input",
+    name: "use",
+    message: "Please enter the use of this project",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Which license would you like to use?",
+    choices: ["MIT", "APACHE 2.0", "GPL", "None"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "How should the user go about adding a contribution?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "How was this project tested?",
+  },
+  {
+    type: "input",
+    name: "questions",
+    message:
+      "Please enter the URL to your gitHub profile, so you can be contacted for questions",
+  },
+];
 
-// function to write README file
-function writeToFile(fileName, data) {}
+function writeFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
-// function to initialize program
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((response) => {
+      console.log(`Generating README...`);
+      writeFile("READMEgen.md", generateMarkdown({ ...response }));
+    })
+    .catch((err) => console.error(err));
+}
 
-// function call to initialize program
 init();
